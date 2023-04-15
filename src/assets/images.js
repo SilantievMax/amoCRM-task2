47792f91c96868151e8074c3995cc6326feb5585
SilantievMax <nohrin.max@yandex.ru>
2023-04-15 12:33:35 +0300

    added backgroud in site

---
+++ src/assets/images.js
@@ -0,0 +1,7 @@
+import purpleball from 'assets/img/purple_ball.png'
+import purpleLight from 'assets/img/purple_light.png'
+import redBallBig from 'assets/img/red_ball_big.png'
+import redBallSmall from 'assets/img/red_ball_small.png'
+import redLight from 'assets/img/red_light.png'
+
+export { purpleball, purpleLight, redBallBig, redBallSmall, redLight }
